6e61746db8ab4ca704e5ffc03e7234e9d7a237bc
awannawa <kurniawanofficiall@gmail.com>
2025-01-21 15:50:30 +0700

    coba dipisahin

--- js/awan.js
+++ js/awan.js
@@ -58,10 +58,16 @@ $(document).ready(function () {
   });
 });
 
-window.addEventListener("load", function () {
+// awan.js
+
+// Fungsi untuk memperbarui gambar
+function updateImages() {
   const images = document.querySelectorAll(".gdrive-image");
   images.forEach(function (img) {
     const imageId = img.getAttribute("idLink");
     img.src = `https://drive.google.com/thumbnail?id=${imageId}&sz=s1080`;
   });
-});
+}
+
+// Event listener untuk memanggil fungsi updateImages saat window load
+window.addEventListener("load", updateImages);
