43a964ea33f1a73035500da3b87f66c6cbd0af6f
awannawa <kurniawanofficiall@gmail.com>
2025-01-30 16:27:35 +0700

    redirect to new portofolio

--- js/awan.js
+++ js/awan.js
@@ -1,73 +1,13 @@
-/*hamburger*/
-$(document).ready(function () {
-  $(".first-button").on("click", function () {
-    $(".animated-icon1").toggleClass("open");
-  });
-  $(".second-button").on("click", function () {
-    $(".animated-icon2").toggleClass("open");
-  });
-  $(".third-button").on("click", function () {
-    $(".animated-icon3").toggleClass("open");
-  });
-});
-/*hamburger*/
+window.onload = function () {
+  let countdown = 5;
+  const countdownElement = document.getElementById("countdown");
 
-/*Scroll ilang*/
-$(function () {
-  var documentEl = $(document),
-    fadeElem = $(".scroll-ilang");
-
-  documentEl.on("scroll", function () {
-    var currScrollPos = documentEl.scrollTop();
-
-    fadeElem.each(function () {
-      var $this = $(this),
-        elemOffsetTop = $this.offset().top;
-      if (currScrollPos > elemOffsetTop)
-        $this.css("opacity", 1 - (currScrollPos - elemOffsetTop) / 800);
-    });
-  });
-});
-/*Scroll ilang*/
-
-/*smoth scroll*/
-$(".page-scroll").on("click", function (e) {
-  var tujuan = $(this).attr("href");
-
-  var elemenTujuan = $(tujuan);
-
-  $("html,body").animate(
-    {
-      scrollTop: elemenTujuan.offset().top - 0,
-    },
-    1250,
-    "easeInOutExpo"
-  );
-
-  e.preventDefault();
-});
-/*END smoth scroll*/
-
-$(document).ready(function () {
-  var filterizd = $(".filtr-container").filterizr({
-    filter: "satu",
-  });
-  $(".filterListItem").on("click", function () {
-    $(".filterListItem").removeClass("active");
-    $(this).addClass("active");
-  });
-});
-
-// awan.js
-
-// Fungsi untuk memperbarui gambar
-function updateImages() {
-  const images = document.querySelectorAll(".gdrive-image");
-  images.forEach(function (img) {
-    const imageId = img.getAttribute("idLink");
-    img.src = `https://drive.google.com/thumbnail?id=${imageId}&sz=s1080`;
-  });
-}
-
-// Event listener untuk memanggil fungsi updateImages saat window load
-window.addEventListener("load", updateImages);
+  const interval = setInterval(function () {
+    countdown--;
+    countdownElement.textContent = countdown;
+    if (countdown === 0) {
+      clearInterval(interval);
+      window.location.href = "https://awannawa.github.io/portofolio";
+    }
+  }, 1000);
+};
--- konfirmasd123.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
-define('6Lcjjr0UAAAAALNG6t8Umv_dD5xtHP6VU8comOGt',"6Lcjjr0UAAAAALNG6t8Umv_dD5xtHP6VU8comOGt"); 
-define('6Lcjjr0UAAAAAMVaMi-t3aPw74cqrIgDE9o_-Lpl',"6Lcjjr0UAAAAAMVaMi-t3aPw74cqrIgDE9o_-Lpl");
-
-?>
